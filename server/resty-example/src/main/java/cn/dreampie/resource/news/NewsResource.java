03e17ed91bfcd9ff1251d5b885e403460b205612
zhouyijiaren <956952610@qq.com>
2016-08-07 17:55:25 +0800

    111

--- server/resty-example/src/main/java/cn/dreampie/resource/news/NewsResource.java
+++ server/resty-example/src/main/java/cn/dreampie/resource/news/NewsResource.java
@@ -10,15 +10,21 @@ import cn.dreampie.route.annotation.API;
 import cn.dreampie.route.annotation.DELETE;
 import cn.dreampie.route.annotation.GET;
 import cn.dreampie.security.Subject;
+import cn.dreampie.utils.MyLogger;
 
 @API("/news")
 public class NewsResource extends ApiResource{
 
-	 @GET(des = "得到首页新闻")
-	  public List getNews() {
-	    String sql = "select * from t_news limit 0,10";
-	    return News.dao.findColsAll("id,title,address,image");
+	 @GET(des = "得到首页新闻",value = "/:page")
+	  public List getNews(int page) {
+		MyLogger.log(page+"");
+	    String sql = "select * from t_news limit ?,?";
+	    return  News.dao.find(sql, page*10-10,10);
 	  }
-
-	  
+	 
+	 @GET(des = "得到首页新闻第一页")
+	  public List getNewsFirst() {
+	    String sql = "select * from t_news limit ?,?";
+	    return News.dao.find(sql, 0,10);
+	  }
 }
